0a3e41a3f7f3ab289e93143565bf7eaa33502ac7
Lurzapps <lurzapps@gmail.com>
2020-12-10 19:31:45 +0100

    normalizing

---
+++ lib/util/utils.dart
@@ -0,0 +1,37 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+
+const version = '1.0';
+const app_name = 'todo';
+
+final Random random = Random();
+
+Future<String> get rootDir async {
+  final directory = await getApplicationDocumentsDirectory();
+  // For your reference print the AppDoc directory
+  return directory.path;
+}
+
+class ContextSingleton {
+  static ContextSingleton _instance;
+  final BuildContext _context;
+
+  ContextSingleton(this._context) {
+    _instance = this;
+  }
+
+  static get context {
+    return _instance._context;
+  }
+}
+
+void addLicenses() {
+  LicenseRegistry.addLicense(() async* {
+    yield LicenseEntryWithLineBreaks(['modal_progress_hud'],
+        await rootBundle.loadString('res/licenses/modal_progress_hud'));
+  });
+}
