658700159651f919a936e83d09bf1ab27ac9969f
Lurzapps <lurzapps@gmail.com>
2020-12-21 00:52:01 +0100

    load the projects

--- lib/holder/todo.dart
+++ lib/holder/todo.dart
@@ -1,29 +1,38 @@
 class TodoItem {
-  String title, id;
-  int deadline;
+  String title;
+  int deadline, created;
 
-  TodoItem(this.id, this.title, this.deadline);
-
-  @override
-  int get hashCode => id.hashCode;
-
-  @override
-  bool operator ==(Object other) {
-    return (other is TodoItem) && (other.id == id);
-  }
+  TodoItem(this.title, this.deadline, this.created);
 }
 
 class TodoTab {
-  String title, id;
-  List<TodoItem> items = [];
+  String title;
+  List<TodoItem> items = [], filteredItems = [];
 
-  TodoTab(this.id, this.title, this.items);
+  TodoTab(this.title, this.items);
 
-  @override
-  int get hashCode => id.hashCode;
-
-  @override
-  bool operator ==(Object other) {
-    return (other is TodoItem) && (other.id == id);
+  void sort(int sortingType) {
+    _sort0(items, sortingType);
+    _sort0(filteredItems, sortingType);
   }
+
+  void _sort0(List<TodoItem> pro, int sortingType) {
+    switch (sortingType) {
+      case TodoItemSortingType.added:
+        pro.sort((a, b) => b.created.compareTo(a.created));
+        break;
+      case TodoItemSortingType.deadline:
+        pro.sort((a, b) => b.deadline.compareTo(a.deadline));
+        break;
+      case TodoItemSortingType.name:
+        pro.sort((a, b) => a.title.compareTo(a.title));
+        break;
+      default:
+        break;
+    }
+  }
+}
+
+class TodoItemSortingType {
+  static const int deadline = 0, name = 1, added = 2;
 }
